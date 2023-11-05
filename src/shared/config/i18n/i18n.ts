2dc80570b4fb4c6d5da997504f3972ea8a4c06e3
dmtrack <rightrack888@gmail.com>
2023-11-05 22:54:43 +0500

    chore: test

--- src/shared/config/i18n/i18n.ts
+++ src/shared/config/i18n/i18n.ts
@@ -4,9 +4,6 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import common_ru from '../../../app/locales/ru/translation.json';
-import common_en from '../../../app/locales/en/translation.json';
-
 i18n.use(Backend)
     .use(LanguageDetector)
     .use(initReactI18next)
@@ -14,14 +11,7 @@ i18n.use(Backend)
     .init({
         fallbackLng: 'ru',
         debug: __IS_DEV__,
-        resources: {
-            ru: {
-                translation: common_ru,
-            },
-            en: {
-                translation: common_en,
-            },
-        },
+
         interpolation: {
             escapeValue: false,
         },
